ed7fe8b0ba592fbce15579c37e48faba174cbc1e
mihaiflorin01194 <mihaiflorin01194@yahoo.com>
2026-05-29 15:18:49 +0300

    Added the class libraries for whole solution and first entities

---
+++ backend/Domain/Entities/Subscriber.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
